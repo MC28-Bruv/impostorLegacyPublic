b1e904b992714003fd8117d0aac8f445a09f4b2a
MC28-Bruv <mc28dude@gmail.com>
2026-09-15 21:29:07 -0400

    ok nvm loadsong can be static

--- source/funkin/states/FreeplayState.hx
+++ source/funkin/states/FreeplayState.hx
@@ -803,7 +803,7 @@ class FreeplayState extends AmongUIState
 		});
 	}
 	
-	public function loadSong(song:String, silent:Bool = false):Void
+	public static function loadSong(song:String, silent:Bool = false):Void
 	{
 		// PlayState.storyMeta.difficulty = 1; // This would be 2 but I just made them all normal difficulty because -hard was annoying lol
 		
